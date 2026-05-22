5fcbf88ce8eda3c54ec0b1315ab92806ba7dfc59
THT-Thenabadu <tathikathenabadu1@gmail.com>
2026-05-22 17:14:45 +0530

    Hero-Section(update)

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -2,16 +2,66 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation"
-import { Search, Calendar, Users, Sparkles } from "lucide-react"
+import { Search, Calendar, Users, Sparkles, MoveRight } from "lucide-react"
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import React from "react";
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc);
+
 
 export default function HeroSection() {
+    const [value, setValue] = React.useState(dayjs.utc('2022-04-17T15:30'));
+
+
+
     return(
         <div className="relative">
             <img className="w-full h-[120vh] object-cover" src="https://lh3.googleusercontent.com/aida-public/AB6AXuAs0TZwlcVaA5s-qF4w-NBQYl5BclQlS0oxxxSrDunc4bq_q8AiwTs4DklYK8ton22DHXECTOZqescgcI8YWk27d8FZoVekG78C8jSWTZCYtDRpV_puJ9ELSDO4wneSA_MOaCEj-8Sv2ZKay6DOHhG7eBld-JENmdUR5PJ3Kvv6HBggqEpYzcUwW5OgceoPGTKbkdz4RXv7Z6asNTjzNPvwD-qMcKUb6Q0EMZpyju7vKKgHEMHMC6_XC3NKJtP2sCGcDWqxiCfnOSQ" />
-            <div className="font-serif absolute top-90 left-20  h-50 w-190 p-5 backdrop-blur-lg rounded-[10px] bg-white/15">
-                <h1 className="text-white text-[20px]">Architectural Precision in Every reservation</h1>
+            <div className="font-serif absolute top-90 left-20  h-70 w-190 p-5 backdrop-blur-lg rounded-[10px] bg-white/15">
+                <h1 className="text-white text-[50px]">Architectural Precision in Every reservation</h1>
+                <button className="font-serif w-50 h-10  justify-center rounded-xl bg-[#001B3C] text-white p-1
+                                shadow-md transition-all duration-300 ease-out hover:-translate-y-1 hover:shadow-xl active:translate-y-0 active:shadow-md flex gap-x-2 items-center relative top-[15px] ">
+                    Plan your event <MoveRight />
+                </button>
+
+                     <div className="w-[92vw] h-30 bg-white relative top-50 right-10 flex justify-between items-top p-3 rounded-sm ">
+                        <div className="w-60   h-5 relative left-5">
+                            <label className="">Date picker</label>
+
+                            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <Stack spacing={2}>
+                                <DateTimePicker value={value} onChange={setValue} timezone="UTC" />
+                            </Stack>
+                            </LocalizationProvider>
+                          
+
+
+                        </div>
+                        <div className="w-20 h-5">
+                            <label>
+                                Event Type
+                            </label>
+                           
+                        </div>
+                        <div className="w-25 h-5">
+                            <label>
+                                Guest count
+                            </label>
+                        </div>
+                        <button>
+                            Click
+                        </button>
+                    </div>
 
             </div>
+           
         </div>
     )
+    
 }
